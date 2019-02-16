79791b8329d5ff55301b5db222ba929448e3f70f
Aditya Verma <adityasinghverma314@gmail.com>
2019-02-16 12:20:38 +0530

    Update JsonParser.java

--- JsonParser.java
+++ JsonParser.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.databind.*;
 import java.util.*;
 import java.io.*;
 
+
+//https://fasterxml.github.io/jackson-databind/javadoc/2.2.0/com/fasterxml/jackson/databind/JsonNode.html#asText()
 public class JsonParser {
 
     public static void main( String [] args){
@@ -13,6 +15,7 @@ public class JsonParser {
             objectMapper = new ObjectMapper();
             rootNode = objectMapper.readTree(new File("nested.json"));
 					
+			//example queries
 			String [] query1 = {"batters", "bamboo"};
 			String [] query2 = {"batters", "bamboo", "hi"};
 			String [] query3 = {"batters", "bamboo", "random"};
@@ -22,6 +25,8 @@ public class JsonParser {
 			JsonNode pointerNode = rootNode;
 			for ( String i : query4 ) {
 					pointerNode = pointerNode.path(i);
+				
+					//remove this s.o.p.
 					System.out.println ( pointerNode.toString() + "\n\n" );
 			
 			
